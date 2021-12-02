f3ac5add3c60b6d8acd05fb3f80dcf388839bba1
Dariia Kachalova <danniednepr@gmail.com>
2021-12-03 00:33:38 +0100

    showing odds and evens in console

--- js/script.js
+++ js/script.js
@@ -3,11 +3,22 @@ const btn = document.getElementById('generate');
 
 
 const arr = [];
+
+
 while(arr.length < 20){
     var r = Math.floor(Math.random() * 100) + 1;
     if(arr.indexOf(r) === -1) arr.push(r);
 }
 console.log(arr);
+var evens = arr.filter((r) => {
+	return r % 2 === 0;
+})
+console.log(evens)
+
+var odds = arr.filter((r) => {
+	return r % 2 === 1;
+})
+console.log(odds)
 
 btn.addEventListener('click', () => {
 	num.innerHTML = arr; 
