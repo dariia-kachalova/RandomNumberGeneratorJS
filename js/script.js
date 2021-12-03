f894341c55781b551ce2568baa39e7c9f4817b0a
Dariia Kachalova <danniednepr@gmail.com>
2021-12-03 22:22:03 +0100

    evens and odds

--- js/script.js
+++ js/script.js
@@ -1,25 +1,47 @@
 const num = document.getElementById('number');
 const btn = document.getElementById('generate');
+const evens = document.getElementById('evens');
+const odds = document.getElementById('odds');
 
 
 const arr = [];
 
 
-while(arr.length < 20){
-    var r = Math.floor(Math.random() * 100) + 1;
-    if(arr.indexOf(r) === -1) arr.push(r);
+while (arr.length < 20) {
+	var r = Math.floor(Math.random() * 100) + 1;
+	if (arr.indexOf(r) === -1) arr.push(r);
 }
-console.log(arr);
-var evens = arr.filter((r) => {
+//console.log(arr);
+
+
+var lol = [] = arr.filter((r) => {
 	return r % 2 === 0;
 })
-console.log(evens)
+arr.push[lol]
 
-var odds = arr.filter((r) => {
+lol.sort(function (a, b) {
+
+	return a - b;
+});
+
+
+var olo = [] = arr.filter((r) => {
 	return r % 2 === 1;
 })
-console.log(odds)
+arr.push[olo]
+
+olo.sort(function (a, b) {
+
+	return a - b;
+});
+
+
+					
+
 
 btn.addEventListener('click', () => {
-	num.innerHTML = arr; 
+//num.innerHTML = arr;
+	evens.innerHTML = lol;
+	odds.innerHTML = olo;
+	
 });
